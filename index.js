ea22ba4bb55f03de044ba793433644e0b7ae9035
graemeerickson <graemeerickson@gmail.com>
2018-08-23 22:36:42 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var morgan = require('morgan');
+var db = require('./models');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(require('morgan')('dev'));
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/public/'));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.use('/tacos', require('./controllers/tacos'));
+
+var server = app.listen(3000);
+
+module.exports = server;
